fc55deebde3b8abdfd023f99b0f9b51427bd67b2
Douglas Francis <Douglas@PoshOps.io>
2023-10-03 16:33:53 -0500

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,5 +1,5 @@
 module "security_group" {
-  source = "git::https://github.com/Coalfire-CF/terraform-aws-securitygroup"
+  source = "https://github.com/Coalfire-CF/terraform-aws-securitygroup"
 
   name        = "${var.name}-sg"
   description = var.sg_description
